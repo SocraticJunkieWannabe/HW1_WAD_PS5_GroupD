9c4686436688e536bbd77640b83d375a63ccec95
Mirky-Shower <44305773+SocraticJunkieWannabe@users.noreply.github.com>
2025-10-28 18:55:46 +0200

    created post json, created script to load and edited template

--- docs/src/posts_loader.js
+++ docs/src/posts_loader.js
@@ -1,29 +1,57 @@
-const data = [
-      {text: 'Debate of the day: Võiga või võita ?', imagePath: "", 
-        date: "20 October 2025", pfpPath: "imgs/blank_profile.webp"},
-      {text: 'Bunch of bricks nothing much...', imagePath: "imgs/posts_media/cathedral.jpg", 
-        date: "20 November 2025", pfpPath: "imgs/blank_profile.webp"},
-      {text: 'New year not new me :(', imagePath: "imgs/posts_media/ulikool.jpeg", 
-        date: "20 December 2025", pfpPath: "imgs/blank_profile.webp"},
-      {text: 'Slava Urkaini', imagePath: "imgs/posts_media/bridge.jpeg", 
-        date: "20 June 2025", pfpPath: "imgs/blank_profile.webp"},
-      {text: 'Anyone got milk, I just ran out ...', imagePath: "", 
-        date: "20 July 2025", pfpPath: "imgs/blank_profile.webp"}
-    ];
+
+
+function fetchOnlineJSON()
+{
+  fetch('https://api.jsonbin.io/v3/b/68ff2c4bae596e708f2fd854')
+  .then((response) => response.json())
+  .then(json => {
 
     const container = document.getElementById('posts-container');
     const template = document.getElementById('post-template');
 
-    data.forEach(item => {
+    json["record"].forEach(item => {
       // Clone the template content
       const clone = template.content.cloneNode(true);
       
       // Fill in the data
-      clone.querySelector('.post-description').textContent = item.text;
-      clone.querySelector('.post-image').src = item.imagePath;
-      clone.querySelector('.post-author-pfp').src = item.pfpPath;
-      clone.querySelector('.post-date').textContent = item.date;
+      clone.querySelector('.post-description').textContent = item.body;
+      clone.querySelector('.post-image').src = item.img;
+      clone.querySelector('.post-author-pfp').src = item.profile_picture;
+      clone.querySelector('.post-date').textContent = item.create_time;
+      clone.querySelector('.username').textContent = item.author_name;
 
       // Append to container
       container.appendChild(clone);
     });
+  });
+}
+
+function fetchLocalJSON()
+{
+  fetch('json/posts.json')
+  .then((response) => response.json())
+  .then(json => {
+
+    const container = document.getElementById('posts-container');
+    const template = document.getElementById('post-template');
+
+    json.forEach(item => {
+      // Clone the template content
+      const clone = template.content.cloneNode(true);
+      
+      // Fill in the data
+      clone.querySelector('.post-description').textContent = item.body;
+      clone.querySelector('.post-image').src = item.img;
+      clone.querySelector('.post-author-pfp').src = item.profile_picture;
+      clone.querySelector('.post-date').textContent = item.create_time;
+      clone.querySelector('.username').textContent = item.author_name;
+
+      // Append to container
+      container.appendChild(clone);
+    });
+  });
+}
+
+fetchLocalJSON()
+
+//fetchOnlineJSON()
